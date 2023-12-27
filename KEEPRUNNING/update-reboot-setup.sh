bfa5b85d7b3029d8bccb7d9f8bcabf9fcf63155c
blackeagle8100 <87874154+blackeagle8100@users.noreply.github.com>
2023-12-27 03:39:36 +0100

    Add files via upload

---
+++ KEEPRUNNING/update-reboot-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+password="ChangeMe"
+echo "Copying necessary update-reboot files..."
+echo "$password" | su -c "cp update-reboot.service /etc/systemd/system"
+echo "$password" | su -c "cp update-reboot.timer /etc/systemd/system"
+echo "Activating timer"
+echo "$password" | su -c "systemctl daemon-reload"
+echo "$password" | su -c "systemctl start update-reboot.timer"
+echo "$password" | su -c "systemctl enable update-reboot.timer"
