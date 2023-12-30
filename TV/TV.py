ca4b58afde2b6a7054a7b6e478c17a506e85c9ec
blackeagle8100 <87874154+blackeagle8100@users.noreply.github.com>
2023-12-30 15:33:09 +0100

    Add files via upload

---
+++ TV/TV.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 02:07:30 2023
+
+@author: meme
+"""
+
+import sys
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+import configparser
+import os
+import subprocess
+from PIL.ImageQt import ImageQt
+from PIL import Image
+
+user_home = os.path.expanduser("~")
+vastsysteem_path = os.path.join(user_home, "VASTSYSTEEM")
+
+config = configparser.ConfigParser()
+config.read(vastsysteem_path + '/settings.ini')
+
+colorcode = config.get('Settings', 'colorcode')
+screen_width = int(config.get('Settings', 'screen_width'))
+screen_height = int(config.get('Settings', 'screen_height'))
+font_size = int(config.get('Settings', 'font_size'))
+font_type = config.get('Settings', 'font_type')
+
+
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+        
+
+class MainWindow(QMainWindow):
+    def load_images(self):
+       folder_path = vastsysteem_path + "/TV/icons"  # Update the folder path here
+
+       if folder_path:
+           self.images = []
+
+           for filename in sorted(
+               os.listdir(folder_path),
+               key=lambda x: os.path.basename(x),  # Sort by file name (alphabetically)
+           ):
+               if filename.endswith((".png", ".jpg", ".jpeg", ".gif")):
+                   image_path = os.path.join(folder_path, filename)
+                   self.images.append(image_path)
+
+           self.show_current_batch()
+           
+          
+    def goto(self, image_path):
+        split = image_path.split('/')
+        naam = split[6]  
+        split = naam.split('.')
+        naam = split[0]
+        split = naam.split('_')
+        naam = split[1]
+        print(naam)
+        subprocess.call(["python3",vastsysteem_path + "/TV/TVbrowser.py", naam])
+        
+        
+        
+    
+  
+    def show_current_batch(self):
+        image_height = 340
+        image_width = 550
+    
+        # Clear the grid layout before adding new widgets
+        for i in reversed(range(self.grid_layout.count())):
+            self.grid_layout.itemAt(i).widget().setParent(None)
+    
+        total_images = len(self.images)
+        for i in range(3):
+            for j in range(3):
+                index = i * 3 + j
+                image_index = self.current_batch_start + index
+                if image_index < total_images:
+                    image_path = self.images[image_index]
+                    image_label = QLabel(self)
+                    pixmap = QPixmap(image_path).scaled(image_width, image_height)
+                    image_label.setPixmap(pixmap)
+                    image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                    image_label.mousePressEvent = lambda event, path=image_path: self.goto(path)
+                    self.grid_layout.addWidget(image_label, i, j)
+                else:
+                    # If there are fewer than 9 images in the current batch, fill the rest with Color widgets
+                    color_widget = Color(colorcode)
+                    color_widget.setFixedSize(image_width, image_height)# You can change 'red' to any color you want
+                    self.grid_layout.addWidget(color_widget, i, j)
+
+
+    def show_previous_batch(self, event=None):
+        if self.current_batch_start > 0:
+            self.current_batch_start -= 9
+            self.current_batch_end -= 9
+            self.grid_layout.deleteLater()
+            self.grid_layout = QGridLayout()
+            self.create_widgets()
+            self.show_current_batch()
+
+    def show_next_batch(self, event=None):
+        if self.current_batch_end < len(self.images):
+            self.current_batch_start += 9
+            self.current_batch_end += 9
+            self.grid_layout.deleteLater()
+            self.grid_layout = QGridLayout()
+            self.create_widgets()
+            self.show_current_batch()
+        
+
+
+
+
+    def create_widgets(self):
+        # CLOSE BUTTON AANMAKEN
+        self.close_button = QPushButton("Sluiten", self)
+        self.close_button.setFont(QFont(font_type, font_size))
+        self.close_button.clicked.connect(self.close)
+        self.close_button.setStyleSheet("background-color: %s;" % colorcode)
+
+        # BACK BUTTON AANMAKEN
+        self.path2 = vastsysteem_path + "/icons/arrowleft.png"
+        self.back_button = QLabel("", self)
+        self.back_button.setFont(QFont(font_type, font_size))
+        #self.back_button.clicked.connect(self.show_previous_batch)
+        im = Image.open(vastsysteem_path + r"/icons/arrowleft.png") 
+        newsize = (115,screen_height-50)
+        cropped_image = im.resize(newsize)
+        q_image = ImageQt(cropped_image)  # Convert PIL image to QImage
+        pixmap = QPixmap.fromImage(q_image)  # Convert QImage to QPixmap     
+        self.back_button.setPixmap(pixmap)   
+        self.back_button.setMouseTracking(True)
+        #self.labelUitvoeren.setCursor(Qt.PointingHandCursor)
+        # Connect the label's clicked event to the uitvoeren method
+        self.back_button.mousePressEvent = self.show_previous_batch
+        self.back_button.setStyleSheet("background-color: %s;" % colorcode)
+
+        # FORWARD BUTTON AANMAKEN
+        self.path3 = vastsysteem_path + "/icons/arrowright.png"
+        self.forward_button = QLabel("", self)
+        self.forward_button.setFont(QFont(font_type, font_size))
+        #self.forward_button.clicked.connect(self.show_next_batch)
+        pixmap = QPixmap(self.path3)
+        
+        im = Image.open(vastsysteem_path + r"/icons/arrowright.png") 
+        newsize = (115,screen_height-50)
+        cropped_image = im.resize(newsize)
+    
+       
+        q_image = ImageQt(cropped_image)  # Convert PIL image to QImage
+        pixmap = QPixmap.fromImage(q_image) 
+        
+        self.forward_button.setPixmap(pixmap)   
+        self.forward_button.setMouseTracking(True)
+        #self.labelUitvoeren.setCursor(Qt.PointingHandCursor)
+        # Connect the label's clicked event to the uitvoeren method
+        self.forward_button.mousePressEvent = self.show_next_batch
+       
+        self.forward_button.setStyleSheet("background-color: %s;" % colorcode)
+
+        self.grid_layout = QGridLayout()
+
+        lh = QHBoxLayout()
+        lh.addStretch()
+        lh.addWidget(self.back_button)
+        lh.addLayout(self.grid_layout)
+        lh.addWidget(self.forward_button)
+
+        lv = QVBoxLayout()
+        lv.addWidget(self.close_button)
+        lv.addLayout(lh)
+
+        layout = QVBoxLayout()
+        layout.addLayout(lv)
+
+        widget = QWidget(self)
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.showFullScreen()
+        self.setWindowTitle("TV and music")
+
+        self.images = []
+        self.current_batch_start = 0
+        self.current_batch_end = 8
+
+        self.create_widgets()
+        self.load_images()
+
+        self.setStyleSheet("background-color: %s;" % colorcode)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
---
+++ TV/TVbrowser.py
@@ -0,0 +1,400 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 16:31:09 2023
+
+@author: meme
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 17:26:11 2023
+
+@author: meme
+"""
+
+import tkinter as tk
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+import os
+import time
+import subprocess
+
+import configparser
+from tkinter import font
+import pyautogui
+import sys
+
+global volume, accX, accH
+volume = 50
+accX = 1145
+accH = 887
+
+user_home = os.path.expanduser("~")
+vastsysteem_path = os.path.join(user_home, "VASTSYSTEEM")
+
+config = configparser.ConfigParser()
+config.read(vastsysteem_path + '/settings.ini')
+
+screen_width = config.get('Settings', 'screen_width')
+colorcode = config.get('Settings', 'colorcode')
+font_size = config.get('Settings', 'font_size')
+font_type = config.get('Settings', 'font_type')
+chrome_options = Options()
+
+#program = 'radio1classic'
+program = sys.argv[1]
+
+def addarguments():
+    # Create an instance of the WebDriver
+    #chromedriver_path = '/home/meme/Documenten/chromedriver'
+    #Disable automate test infobar
+    chrome_options.add_argument('--disable-infobars')
+    
+    # Automatically accept all cookies
+    chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--disable-web-security')
+    chrome_options.add_argument('--disable-site-isolation-trials')
+    chrome_options.add_argument("--enable-automation")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    
+    
+    #chrome_options.add_argument("--app="+url)
+
+
+
+def createbrowser(url):
+        
+    global driver
+    driver = webdriver.Chrome(options= chrome_options)
+    
+    #driver = webdriver.Chrome(executable_path='/home/meme/VASTSYSTEEM/chromedriver', options=chrome_options)
+    driver.maximize_window()
+    
+    driver.get(url)
+
+
+time.sleep(2)
+def vrtmaxplay():
+    time.sleep(2)
+   
+    pyautogui.click(accX, accH) #alles accepteren
+    elem = driver.find_element(By.CLASS_NAME, "SRSOVR")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "CrD3n2")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "skQsGK")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "mPKpBH")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "EobK_g")
+    driver.execute_script("arguments[0].remove();", elem)
+    
+    
+    
+    elem = driver.find_element(By.CLASS_NAME, "baTnBX")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "HL8Eec")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "GJXJ7X")
+    driver.execute_script("arguments[0].style.width = '1500px';", elem)
+    driver.execute_script("arguments[0].style.height = '750px';", elem)
+    driver.execute_script("arguments[0].style.position = 'fixed'; \
+                           arguments[0].style.top = '50%'; \
+                           arguments[0].style.left = '50%'; \
+                           arguments[0].style.transform = 'translate(-50%, -50%)';", elem)
+    time.sleep(2)
+    pyautogui.click(955, 602) #press play
+
+
+def vrtmaxkijk():
+    
+    time.sleep(2)
+    pyautogui.click(accX, accH) #alles accepteren
+    elem = driver.find_element(By.CLASS_NAME, "SRSOVR")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "CrD3n2")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "skQsGK")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "mPKpBH")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "EobK_g")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "baTnBX")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "HL8Eec")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "GJXJ7X")
+    driver.execute_script("arguments[0].style.width = '1700px';", elem)
+    driver.execute_script("arguments[0].style.height = '950px';", elem)
+    driver.execute_script("arguments[0].style.position = 'fixed'; \
+                           arguments[0].style.top = '50%'; \
+                           arguments[0].style.left = '50%'; \
+                           arguments[0].style.transform = 'translate(-50%, -50%)';", elem)  
+    time.sleep(2)
+    pyautogui.click(949, 653) #press play
+    
+def vrtmaxluister():
+    
+    time.sleep(2)
+    pyautogui.click(accX, accH) #alles accepteren
+    
+    elem = driver.find_element(By.CLASS_NAME, "SRSOVR")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "CrD3n2")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "skQsGK")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "mPKpBH")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "EobK_g")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "baTnBX")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "HL8Eec")
+    driver.execute_script("arguments[0].remove();", elem)
+    elem = driver.find_element(By.CLASS_NAME, "GJXJ7X")
+    driver.execute_script("arguments[0].style.width = '1700px';", elem)
+    driver.execute_script("arguments[0].style.height = '950px';", elem)
+    driver.execute_script("arguments[0].style.position = 'fixed'; \
+                           arguments[0].style.top = '50%'; \
+                           arguments[0].style.left = '50%'; \
+                           arguments[0].style.transform = 'translate(-50%, -50%)';", elem)   
+    time.sleep(2)
+    pyautogui.click(957, 399) #press play                     
+                           
+def set_volume(volume_level):
+    # Calculate the volume value based on the desired level
+    volume = int(volume_level)  # Range: 0-100
+
+    # Set the volume using the 'amixer' command
+    subprocess.run(["amixer", "-D", "default", "sset", "Master", f"{volume}%"])
+
+
+# voeg een functie toe aan de volume_down_button
+def volume_down_button_clicked():
+    
+     #driver.execute_script("document.querySelector('video').volume -= 0.1;")
+    global volume
+    if volume == 0:
+        volume = 0
+    else:
+        volume = volume -10
+        set_volume(volume)
+
+# voeg een functie toe aan de volume_up_button
+def volume_up_button_clicked():
+   
+    #driver.execute_script("document.querySelector('video').volume += 0.1;")
+    global volume
+    if volume == 100:
+        volume = 100
+    else:    
+        volume = volume +10
+        set_volume(volume)
+    
+def close():
+
+    try:        
+        driver.quit()
+    except Exception as e:
+        print(e)
+        
+    root.destroy()
+
+
+print(program)
+if program == 'canvas': 
+    url = 'https://www.vrt.be/vrtnu/livestream/video/canvas/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxplay()
+    
+    
+elif program == 'vrt1' or program == 'vrt':
+    url = 'https://www.vrt.be/vrtnu/livestream/video/een/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxplay()
+elif program == 'Ketnet' or program == 'ketnetgroen':
+    url = 'https://www.vrt.be/vrtnu/livestream/video/ketnet/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxplay()
+    
+elif program == 'Ketnet-Junior':
+    url = 'https://www.vrt.be/vrtnu/livestream/video/ketnet-jr/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxplay()
+elif program == 'vrtnews' or program == 'VRT-NWS':
+    url = 'https://www.vrt.be/vrtnu/livestream/video/vgt-journaal/'
+    addarguments()
+    createbrowser(url)  
+    vrtmaxplay()
+elif program == 'MNM':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/mnm/?context=kijk'
+    addarguments()
+    createbrowser(url)
+    vrtmaxkijk()
+elif program == 'stubru':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/stubru/?context=kijk'
+    addarguments()
+    createbrowser(url)
+    vrtmaxkijk()
+elif program == 'radio1':
+    url = 'https://www.vrt.be/vrtnu/livestream/video/radio1/?context=kijk'
+    addarguments()
+    createbrowser(url)
+    vrtmaxkijk()
+elif program == 'radio2':
+    url = 'https://www.vrt.be/vrtnu/livestream/video/radio2/?context=kijk'
+    addarguments()
+    createbrowser(url)
+    vrtmaxkijk()
+elif program == 'radio1classic':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/radio1classics/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+elif program == 'lagelanden':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/radio1lagelandenlijst/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+elif program == 'unwind':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/radio2unwind/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+elif program == 'klara':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/klara/?context=kijk'
+    addarguments()
+    createbrowser(url)
+    vrtmaxkijk()
+elif program == 'klara-continuo':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/klaracontinuo/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+elif program == 'ketnethits':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/ketnethits/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+
+
+elif program == 'mnmtrowback':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/mnmthrowback/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'renbeats':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/mnmrbeats/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'MNM-hits':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/mnmhits/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'tijdloze':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/tijdloze/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'stubru-vuurland':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/vuurland/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'stubru-bruut':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/bruut/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'stubru-untz':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/untz/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'stubru-hooray':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/hooray/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+elif program == 'Radio2-benebene':
+    url = 'https://www.vrt.be/vrtnu/livestream/audio/radio2benebene/'
+    addarguments()
+    createbrowser(url)
+    vrtmaxluister()
+    
+
+
+        
+else:
+    print('programma nog niet ingesteld')
+    url = 'http://www.google.be'
+print(url)
+
+
+
+
+
+
+
+
+
+#zet vulume half in opstart
+set_volume(volume)
+# maak het root-venster
+root = tk.Tk()
+custom_font = font.Font(size=font_size, family= font_type)
+# stel root venster in
+root.overrideredirect(True)
+root.geometry(screen_width +"x160")
+# stel `always on top` in op `true`
+root.wm_attributes("-topmost", True)
+root.configure(bg= colorcode)
+
+hoogte = 2
+
+# maak de volume omlaag-knop
+volume_down_button = tk.Button(root, text="Volume omlaag", font=custom_font , bg= colorcode, activebackground= colorcode, height=hoogte )
+# maak de volume omhoog-knop
+volume_up_button = tk.Button(root, text="Volume omhoog", font=custom_font , bg= colorcode, activebackground= colorcode, height=hoogte )
+# maak de sluit-knop
+close_button = tk.Button(root, text="Sluiten", font=custom_font , bg= colorcode, activebackground= colorcode, height=hoogte )
+
+
+
+# voeg de functie toe aan de volume_down_button
+volume_down_button.config(command=volume_down_button_clicked)
+# voeg de functie toe aan de volume_up_button
+volume_up_button.config(command=volume_up_button_clicked)
+close_button.config(command=close)
+
+
+
+# plaats de knoppen op het root-venster
+volume_down_button.pack(side="left")
+volume_up_button.pack(side="left")
+close_button.pack(side="right")
+
+# initialiseer het root-venster
+root.mainloop()
