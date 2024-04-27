7f3565b058b173bcd5305e366364899dddaaf55f
blackeagle8100 <87874154+blackeagle8100@users.noreply.github.com>
2024-04-27 14:15:22 +0200

    Delete UsefullPrograms directory

--- UsefullPrograms/pip-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-password="YOUR PASSWORD"
-echo "Installing..."
-echo "$password" | su -c "pip install --break-system-packages pytube pyautogui moviepy"
-echo "Finished"
-echo "Copying Cipher.py"
-echo "$password" | su -c "cp cipher.py /usr/local/lib/python3.11/dist-packages/pytube"
-echo "Finished Copying"
-
