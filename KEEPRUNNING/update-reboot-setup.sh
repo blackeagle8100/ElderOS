0205752da95cd98b136d18f5d03ba2b0cfc116cc
blackeagle8100 <87874154+blackeagle8100@users.noreply.github.com>
2024-04-28 17:26:52 +0200

    Add files via upload

--- KEEPRUNNING/update-reboot-setup.sh
+++ KEEPRUNNING/update-reboot-setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-password="CHANGEME!" #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! inifile password sudo?
+
+# Function to decrypt the password using Python script
+decrypt_password() {
+    # Call the Python script to decrypt the password
+    decrypted_password=$(python3 ~/VASTSYSTEEM/dencrypt.py decrypt "$1")
+    echo "$decrypted_password"
+}
+
+password=$(cat ~/VASTSYSTEEM/S)
+decpswd=$(decrypt_password "$password")
 echo "Copying necessary update-reboot files..."
-echo "$password" | su -c "cp update-reboot.service /etc/systemd/system"
-echo "$password" | su -c "cp update-reboot.timer /etc/systemd/system"
+echo "$decpswd" | su -c "cp update-reboot.service /etc/systemd/system"
+echo "$decpswd" | su -c "cp update-reboot.timer /etc/systemd/system"
 echo "Activating timer"
-echo "$password" | su -c "systemctl daemon-reload"
-echo "$password" | su -c "systemctl start update-reboot.timer"
-echo "$password" | su -c "systemctl enable update-reboot.timer"
+echo "$decpswd" | su -c "systemctl daemon-reload"
+echo "$decpswd" | su -c "systemctl start update-reboot.timer"
+echo "$decpswd" | su -c "systemctl enable update-reboot.timer"
--- KEEPRUNNING/update-reboot.sh
+++ KEEPRUNNING/update-reboot.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-password="YOUR_SUDO_PASSWORD"
+
+# Function to decrypt the password using Python script
+decrypt_password() {
+    # Call the Python script to decrypt the password
+    decrypted_password=$(python3 ~/VASTSYSTEEM/dencrypt.py decrypt "$1")
+    echo "$decrypted_password"
+}
+
+password=$(cat ~/VASTSYSTEEM/S)
+decpswd=$(decrypt_password "$password")
+
+
+
 echo "Running apt update and full-upgrade..."
-echo "$password" | su -c "apt update && apt full-upgrade -y"
+echo "$decpswd" | su -c "apt update && apt full-upgrade -y"
 echo "Finished running apt update and full-upgrade."
 echo "Rebooting system..."
 systemctl reboot
