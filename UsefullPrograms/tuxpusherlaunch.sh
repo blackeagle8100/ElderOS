bbcfed054bed51c989037ee6b1af73c554188065
blackeagle8100 <87874154+blackeagle8100@users.noreply.github.com>
2024-05-02 22:25:30 +0200

    Add files via upload

--- UsefullPrograms/tuxpusherlaunch.sh
+++ UsefullPrograms/tuxpusherlaunch.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # Het spel starten
-
-bash -c '/home/sbe/.local/share/flatpak/exports/bin/com.tuxpusher.TuxPusher & xdotool search --sync --onlyvisible --class tuxpusher windowsize 100%% 100%%'
-
+tuxpusher & xdotool search --sync --onlyvisible --class tuxpusher windowsize 100%% 100%%
