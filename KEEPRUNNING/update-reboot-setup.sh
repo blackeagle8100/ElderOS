ef7e550474883404f05a6ddbf9e17011f655a287
Carnat1on <167426837+Carnat1on@users.noreply.github.com>
2024-07-12 21:17:11 +0200

    Update update-reboot-setup.sh

--- KEEPRUNNING/update-reboot-setup.sh
+++ KEEPRUNNING/update-reboot-setup.sh
@@ -10,8 +10,8 @@ pwd
 password=$(cat ~/VASTSYSTEEM/S)
 decpswd=$(decrypt_password "$password")
 echo "Copying necessary update-reboot files..."
-echo "$decpswd" | su -c "cp /VASTSYSTEEM/KEEPRUNNING/update-reboot.service /etc/systemd/system"
-echo "$decpswd" | su -c "cp /VASTSYSTEEM/KEEPRUNNING/update-reboot.timer /etc/systemd/system"
+echo "$decpswd" | su -c "cp VASTSYSTEEM/KEEPRUNNING/update-reboot.service /etc/systemd/system"
+echo "$decpswd" | su -c "cp VASTSYSTEEM/KEEPRUNNING/update-reboot.timer /etc/systemd/system"
 echo "Activating timer"
 echo "$decpswd" | su -c "systemctl daemon-reload"
 echo "$decpswd" | su -c "systemctl start update-reboot.timer"
