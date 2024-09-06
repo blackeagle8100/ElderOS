193dea54bf45820968eab9e19015447a0442a38d
Carnat1on <167426837+Carnat1on@users.noreply.github.com>
2024-09-06 16:15:26 +0200

    Update autostartup.sh

--- autostartup.sh
+++ autostartup.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
 
 vastsysteem-call() {
-    ~/VASTSYSTEEM/ElderOS.py
-}
-cordenhance() {
-    cd ~/VASTSYSTEEM/KEEPRUNNING
-    ./cordEnhance.sh
+    
+    nohup ~/VASTSYSTEEM/ElderOS.py
 }
 
-cordbot() {
-    cd ~/VASTSYSTEEM/KEEPRUNNING
-    ./cordbot.py
-}
-phone() {
-    cd ~/VASTSYSTEEM/Telephone/TELEFOON
-    ./activatephone.py
-}
-watcher() {
-    cd ~/VASTSYSTEEM/Telephone/TELEFOON
-    ./watcher.py 
-}
-batt() {
-    cd ~/VASTSYSTEEM/Telephone/TELEFOON
-    ./battchecker.py
-}
-
-
-vastsysteem-call &
-cordenhance &
-phone &
-watcher &
-batt &
-
-sleep 20
-wmctrl -a MAIN
-cordbot
+vastsysteem-call
 
